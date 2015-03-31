2f0c7f32d11797d1cefbdd27e9c089e4dcfabae2
Joe Newman <makerslaptop23@Makerss-MBP.home>
2015-03-31 21:22:40 +0100

    refactoring

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,12 @@
 require "paperclip/railtie"
 Paperclip::Railtie.insert
+
+Paperclip.interpolates(:s3_eu_url) do |att, style|
+    "#{att.s3_protocol}://s3-eu-west-1.amazonaws.com/#{att.bucket_name}/#{att.path(style)}"
+end
+
+module AWS
+    module S3
+        DEFAULT_HOST = "s3-eu-west-1.amazonaws.com"
+    end
+end
