c7d730d353324f83aa471f823a2530ee4c748ba4
Joe Newman <makerslaptop23@Makerss-MBP.home>
2015-03-31 22:10:52 +0100

    refactoring

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,7 @@ class Restaurant < ActiveRecord::Base
   validates :name, length: {minimum: 3}, uniqueness: true
   belongs_to :user
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :url => ":s3_eu_url" , :default_url => "https://s3-eu-west-1.amazonaws.com/yelppicbucket/:style/missing.png"
-  :default_url => "https://s3-eu-west-1.amazonaws.com/yelppicbucket/:style/missing.png"
+  # :default_url => "https://s3-eu-west-1.amazonaws.com/yelppicbucket/:style/missing.png"
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   
