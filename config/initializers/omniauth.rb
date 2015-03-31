6f63244fa7c7889c0ccc8927a2b1ade27a896228
Joe Newman <makerslaptop23@Makerss-MBP.home>
2015-03-31 23:50:05 +0100

    refactoring

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,2 +1,12 @@
-provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"], 
-           {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"]
+                # :scope => 'email,user_birthday', :display => 'popup'
+  OmniAuth.config.logger = Rails.logger
+end
+
+
+
+
+
+# provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"], 
+           # {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
