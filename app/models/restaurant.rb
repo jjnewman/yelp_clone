254c266ddab600ff6f9cfff97351bf6f9a3cd62c
Joe Newman <makerslaptop23@Makerss-MBP.home>
2015-03-31 22:15:52 +0100

    refactoring

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,8 +2,9 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   validates :name, length: {minimum: 3}, uniqueness: true
   belongs_to :user
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :url => ":s3_eu_url" , :default_url => "https://s3-eu-west-1.amazonaws.com/yelppicbucket/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },  :default_url => "https://s3-eu-west-1.amazonaws.com/yelppicbucket/:style/missing.png"
   # :default_url => "https://s3-eu-west-1.amazonaws.com/yelppicbucket/:style/missing.png"
+  # :url => ":s3_eu_url" ,
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   
