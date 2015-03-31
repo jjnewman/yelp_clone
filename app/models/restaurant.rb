23b5b2570f01ea318999e1b8d7b4c69ebfe6d4ac
Joe Newman <makerslaptop23@Makerss-MBP.home>
2015-03-31 17:58:00 +0100

    devise gems changed again6

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,7 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   validates :name, length: {minimum: 3}, uniqueness: true
   belongs_to :user
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "https://s3-eu-west-1.amazonaws.com/yelppicbucket/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :url => "https://s3-eu-west-1.amazonaws.com/yelppicbucket/:style/:image" , :default_url => "https://s3-eu-west-1.amazonaws.com/yelppicbucket/:style/missing.png"
   
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   
