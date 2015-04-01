8056aa26ece22b722988589b2f479053b9738efa
Joe Newman <makerslaptop23@Makerss-MBP.home>
2015-04-01 01:08:53 +0100

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "authentications" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "user/authentications" }
   # 
 root to: "restaurants#index"
   
