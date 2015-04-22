8867752fd1d32a658ff8858b806d19caf586ece3
Joe Newman <jj_newman@hotmail.com>
2015-04-22 23:12:52 +0100

    Update endorsements_controller.rb

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -4,7 +4,6 @@ class EndorsementsController < ApplicationController
     @review = Review.find(params[:review_id])
     @review.endorsements.create
     render json: {new_endorsement_count: @review.endorsements.count}
-    # redirect_to restaurants_path
   end
 
 end
